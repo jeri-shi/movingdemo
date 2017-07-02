e687720becd7b7d154d7fffd5d6fae5e74dc28ad
shijin <shijin@JeriShi>
2017-07-02 14:14:31 +0800

    complete add jndi datasource

--- src/main/java/com/shijin/learn/movingdemo/DemoApplication.java
+++ src/main/java/com/shijin/learn/movingdemo/DemoApplication.java
@@ -3,7 +3,12 @@
  */
 package com.shijin.learn.movingdemo;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
@@ -11,6 +16,8 @@ import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletCon
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
 
 /**
  * @author shijin
@@ -30,6 +37,7 @@ public class DemoApplication {
   
   /**
    * Initial embedded tomcat with jndi support
+   * @see https://stackoverflow.com/questions/24941829/how-to-create-jndi-context-in-spring-boot-with-embedded-tomcat-container
    * @see https://github.com/wilkinsona/spring-boot-sample-tomcat-jndi/blob/master/src/main/java/sample/tomcat/jndi/SampleTomcatJndiApplication.java
    * @return
    */
@@ -43,8 +51,33 @@ public class DemoApplication {
           return super.getTomcatEmbeddedServletContainer(tomcat);
         }
         
+        @Override
+        protected void postProcessContext(Context context) {
+          ContextResource resource = new ContextResource();
+          resource.setName("jdbc/datasource");
+          resource.setType(DataSource.class.getName());
+          resource.setProperty("driverClassName", "com.mysql.jdbc.Driver");
+          resource.setProperty("url", "jdbc:mysql://localhost:3306/hello");
+          resource.setProperty("username", "root");
+          resource.setProperty("password", "root");
+          resource.setProperty("maxActive", "5");
+          resource.setProperty("maxIdle", "3");
+          resource.setProperty("maxWait", "10000");
+              
+          context.getNamingResources().addResource(resource);
+        }
       };
 
   }
 
+  @Bean(destroyMethod="")
+  @Profile("embedded")
+  public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+      JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+      bean.setJndiName("java:comp/env/jdbc/dataSource");
+      bean.setProxyInterface(DataSource.class);
+      bean.setLookupOnStartup(false);
+      bean.afterPropertiesSet();
+      return (DataSource)bean.getObject();
+  }
 }
