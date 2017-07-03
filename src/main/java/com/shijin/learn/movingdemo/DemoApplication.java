776be35e648bd16c586588b17b372b334095a2ba
shijin <shijin@peer1>
2017-07-03 15:40:35 +0800

    convert spring boot to spring cloud

--- src/main/java/com/shijin/learn/movingdemo/DemoApplication.java
+++ src/main/java/com/shijin/learn/movingdemo/DemoApplication.java
@@ -7,13 +7,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
  * @author shijin
  *
  */
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
   private static final Logger LOGGER = LogManager.getLogger(DemoApplication.class);
@@ -25,4 +29,10 @@ public class DemoApplication {
     SpringApplication.run(DemoApplication.class, args);
 
   }
+  
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+  
 }
